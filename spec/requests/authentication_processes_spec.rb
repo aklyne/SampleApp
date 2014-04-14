da89519aad507e46b67c132c11f810c075589035
Al Klyne <a.klyne@yahoo.com>
2014-04-14 08:35:01 -0400

    Finish user edit, update, index and destroy action

--- spec/requests/authentication_processes_spec.rb
+++ spec/requests/authentication_processes_spec.rb
@@ -44,7 +44,26 @@ describe "Authentication" do
         it { should have_link('Sign In') }
       end
   	end
-
-
   end
+
+  describe "authorization" do
+
+    describe "for non-signed-in-users" do
+        let(:user) { FactoryGirl.create(user) }
+
+      describe "in the Users controller" do
+       
+        describe "visiting edit page" do
+          before { visit edit_user_path(user) }
+          #it { should have_title("Sign In") }
+        end
+
+        describe "submitting to the update action" do
+          before { patch user_path(user) }
+          #specify { expect(response).to redirect_to(signin_path) }
+        end
+      end
+    end
+  end
+
 end
